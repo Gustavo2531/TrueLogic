ea09b62a8261212745e76090a07636d8061b55ba
Gustavo2531 <gustavo31.25@gmail.com>
2021-03-08 21:29:03 -0600

    Try

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -11,5 +11,5 @@ $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
 #Listen Topic
 $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic novice-players --from-beginning
 
-#Create Topic
+#Create Topics
 $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic novice-players --partitions 5 --replication-factor 1 --config segment.bytes=1000000
