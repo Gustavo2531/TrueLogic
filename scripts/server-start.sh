f8bbfd2f9c4c4ef8388933e56adff8b7ae2196d4
Gustavo2531 <gustavo31.25@gmail.com>
2021-03-08 21:12:25 -0600

    Change Scripts

--- scripts/server-start.sh
+++ scripts/server-start.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-export KAFKA_HOME="/Users/gustavomendez/kafka-2.5.0-src/"
+#Start Zookeper
 $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties
 
 
-export KAFKA_HOME="/Users/gustavomendez/kafka-2.5.0-src/"
+#Start Kafka
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
 
-export KAFKA_HOME="/Users/gustavomendez/kafka-2.5.0-src/"
+
+#Listen Topic
 $KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic novice-players --from-beginning
 
-export KAFKA_HOME="/Users/gustavomendez/kafka-2.5.0-src/"
+#Create Topic
 $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic novice-players --partitions 5 --replication-factor 1 --config segment.bytes=1000000
